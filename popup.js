8726a597ceae9796df3964a9e53949e116bae50b
jasminecao <jasminecao03@gmail.com>
2020-04-10 17:29:05 -0400

    get html

--- popup.js
+++ popup.js
@@ -1,15 +1,25 @@
-let importButton = document.getElementById('import');
+document.getElementById("linkedin").addEventListener('click', () => {
+  chrome.tabs.create({active: true, url: 'https://www.linkedin.com/mynetwork/invite-connect/connections/'})
+});
 
-// chrome.storage.sync.get('color', function(data) {
-//   importButton.style.backgroundColor = data.color;
-//   importButton.setAttribute('value', data.color);
-// });
+document.getElementById("import").addEventListener('click', () => {
+  function getDOM() {
+      //check link is linkedin.com?
+      console.log(document.body);
+      let contacts = document.getElementsByClassName("mn-connection-card__name");
+      let display = "";
+      for (let i = 0; i < contacts.length; i++) {
+        contacts[i] = contacts[i].innerHTML;
+        display = display + contacts[i].innerHTML;
+      }
+      alert(display);
+      return contacts;
+  }
 
-// importButton.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
+  chrome.tabs.executeScript({
+      code: '(' + getDOM + ')();'
+  }, (results) => {
+      console.log('Popup script:')
+      console.log(results[0]);
+  });
+});
