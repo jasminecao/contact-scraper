bda02d9425c351d62272aa1749127c016b0f99d0
jasminecao <jasminecao03@gmail.com>
2020-04-16 16:31:12 -0400

    click next button

--- content_script.js
+++ content_script.js
@@ -1,16 +1,27 @@
+function scroll(callback) {
+  window.scrollBy(0, document.body.scrollHeight);
+  window.setTimeout(callback, 1000);
+}
+
 function getContacts() {
-	let elements = document.getElementById('ember65').getElementsByTagName('li');
-	let data = {};
-	for (i = 3; i < elements.length; i++) {
-		scrapeProfile(elements[i]);
-	}
+  let elements = document.getElementById('ember65').getElementsByClassName('search-results__list')[0].getElementsByTagName('li');
+  let data = {};
+  let nextButton = document.getElementsByClassName('artdeco-pagination__button--next')[0];
+  if (!nextButton.disabled) {
+    for (i = 3; i < elements.length; i++) {
+      scrapeProfile(elements[i]);
+    }
+    nextButton.click();
+    console.log('clicked');
+    scroll(getContacts);
+  }
 }
 
 const scrapeProfile = (contact) => {
 	const profileLink = contact.getElementsByClassName(
 		'search-result__result-link ember-view'
   )[0];
-	profileLink.click();
+  //profileLink.click();
 };
 
-getContacts();
+scroll(getContacts);
